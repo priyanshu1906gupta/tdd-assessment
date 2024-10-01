08912f48bc0ae8934f069accc558842e161dfafb
priyanshu-gupta-19 <priyanshu.gupta@krazy-tech.com>
2024-10-01 23:43:24 +0530

    Support custom delimiters

--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -17,9 +17,9 @@ test('should handle new lines as delimiters', () => {
   expect(add("1\n2,3")).toBe(6);
 });
 
-// test('should support custom delimiters', () => {
-//   expect(add("//;\n1;2")).toBe(3);
-// });
+test('should support custom delimiters', () => {
+  expect(add("//;\n1;2")).toBe(3);
+});
 
 // test('should throw an exception for negative numbers', () => {
 //   expect(() => add("1,-2,3")).toThrow("Negative numbers not allowed: -2");
